5f17eccc28ced0de16ed9c74b3f5fa5e4cc1c76b
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2022-09-09 14:29:24 +0200

    Commit_simple2

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,5 +1,12 @@
 <template>
 <div class="p-2">
     <h1 class="text-2xl">Page Simple</h1>
+    <card class="w-1/2" v-bind="objet"/>
 </div>
 </template>
+
+<script setup lang="ts">
+    import card from "../components/card.vue";
+
+    const objet = {nom:"Maison 1", price:256, favoris:false, txt:"Maison 1", nbbath:1, nbsize:"m²", img:"/public/card.jpg"}
+</script>
