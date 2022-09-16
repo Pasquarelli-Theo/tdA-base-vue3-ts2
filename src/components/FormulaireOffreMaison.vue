04a2baed8a300f55fe593ff721a643373a81cbc2
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2022-09-16 15:14:09 +0200

    commit_install_formkit

---
+++ src/components/FormulaireOffreMaison.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import Card from "./card.vue";
+// On fait une variable réactive qui réference les données
+// ATTENTION : faire une Ref pas une Reactive car :
+// c'est l'objet qui doit être réactif, pas ses props
+const maison = ref({});
+</script>
+<template>
+  <div>
+    <div class="p-2">
+      <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
+      <!-- Optionnel on affiche les données -->
+      <Card v-bind="maison" />
+    </div>
+    <div class="p-2">
+      <!-- On passe la "ref" à FormKit -->
+      <FormKit
+        type="form"
+        :submit-attrs="{ classes: { input: 'bg-red-600 p-1 rounded' } }"
+        :config="{
+          classes: {
+            input: 'p-1 rounded border-black shadow-sm border',
+            label: 'text-gray-600',
+          },
+        }"
+        v-model="maison"
+      >
+        <FormKit name="nom" label="Ville" />
+        <FormKit name="txt" label="adresse" />
+        <FormKit name="price" label="prix" type="number" />
+        <FormKit
+          name="favoris"
+          label="mettre en valeur"
+          type="checkbox"
+          wrapper-class="flex"
+        />
+        <FormKit name="nbbath" label="nobre de salle de bain" />
+        <FormKit name="nbsize" label="superficie" />
+      </FormKit>
+    </div>
+  </div>
+</template>
---
+++ src/pages/edit/new.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FormulaireOffreMaison from "../../components/FormulaireOffreMaison.vue";
+</script>
+
+<template>
+  <div class="p-2">
+    <h1 class="text-2xl">Page Formulaire</h1>
+    <FormulaireOffreMaison v-bind="maison" />
+  </div>
+</template>
