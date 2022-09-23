3a89860e7792d7eb46fed23e74857b4894258be3
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2022-09-23 14:17:42 +0200

    debut_test_supabase

---
+++ src/pages/liste-supabase.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-2">
+    <h1 class="text-2xl">Page Liste Supabase</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { supabase } from "../supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+const maisons = []; // à remplacer par l'appel à Supabase
+
+
+</script>
