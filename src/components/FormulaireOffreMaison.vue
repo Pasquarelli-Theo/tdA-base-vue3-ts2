9ab4dc784480a4de87f20448d488f80f80a69bb3
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2022-10-04 20:58:41 +0200

    réparation

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -1,68 +1,55 @@
 <script setup lang="ts">
-import { supabase } from "@/supabase";
-import { ref } from "@vue/reactivity";
-import { useRouter } from "vue-router";
-import Card from "./card.vue";
-// On fait une variable réactive qui réference les données
-// ATTENTION : faire une Ref pas une Reactive car :
-// c'est l'objet qui doit être réactif, pas ses props
-const router = useRouter();
- const Maisons = ref({});
-
-const props = defineProps(["id"]);
+    import { supabase } from "@/supabase";
+    import { ref } from "@vue/reactivity";
+    import { useRouter } from "vue-router";
+    import Card from "./card.vue";
+    const router = useRouter();
+  const props = defineProps(["id"]);
 if (props.id) {
     // On charge les données de la maison
-    let { data, error } = await supabase
+    let { data: Maison, error } = await supabase
         .from("Maison")
         .select("*")
-        .eq("id", props.id);
+        .eq("id_maison", props.id);
     if (error) console.log("n'a pas pu charger le table Maison :", error);
-    else Maisons.value = (data as any[])[0];
+    else Maison.value = (data as any[])[0];
 }
-
-
-async function upsertMaison(dataForm, node) {
-    const { data, error } = await supabase.from("Maisons").upsert(dataForm);
+    async function upsertMaison(dataForm, node) {
+    const { data, error } = await supabase.from("Maison").upsert(dataForm);
     if (error || !data) node.setErrors([error?.message])
     else {
-        node.setErrors([]);
-        router.push({ name: "edit-id", params: { id: data[0].id } });
+    node.setErrors([]);
+    router.push({ name: "edit-id", params: { id_maison: data[0].id} });
     }
 }
 </script>
+
 <template>
-  <div>
-    <div class="p-2">
-      <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
-      <!-- Optionnel on affiche les données -->
-      <Card v-bind="maison" />
+    <div>
+        <div class="p-2">
+            <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
+            <!-- Optionnel on affiche les données -->
+            <Card v-bind="Maison" />
+
+        </div>
+        <div class="p-2">
+            <!-- On passe la "ref" à Formkit -->
+            <FormKit type="form" v-model="Maison" @submit="upsertMaison"
+            :config="{
+                        classes: {
+                                    input: 'p-1 rounded shadow-sm border bg-gray-100',
+                                    label: 'text-gray-600',
+                                },
+                    }"
+            :submit-attrs="{ classes: { input: 'bg-red-600 mt-2 px-6 py-2 rounded-full text-center' } }">
+                <FormKit name="nom" label="Nom"/>
+                <FormKit name="txt" label="Adresse"/>
+                <FormKit name="price" label="Prix" type="number"/>
+                <FormKit name="img" label="Image"/>
+                <FormKit name="nbsize" label="Superficie"/>
+                <FormKit name="nbbath" label="Nombre de salle de bains" type="number"/>
+                <!-- <FormKit name="favoris" label="mettre en valeur" type="checkbox" wrapper-class="flex gap-4"/> -->
+            </FormKit>
+        </div>
     </div>
-    <div class="p-2">
-      <!-- On passe la "ref" à FormKit -->
-      <FormKit
-        type="form"
-        :submit="upsertMaison"
-        :submit-attrs="{ classes: { input: 'bg-red-600 p-1 rounded' } }"
-        :config="{
-          classes: {
-            input: 'p-1 rounded border-black shadow-sm border',
-            label: 'text-gray-600',
-          },
-        }"
-        v-model="maison"
-      >
-        <FormKit name="nom" label="Ville" />
-        <FormKit name="txt" label="adresse" />
-        <FormKit name="price" label="prix" type="number" />
-        <FormKit
-          name="favoris"
-          label="mettre en favorie"
-          type="checkbox"
-          wrapper-class="flex"
-        />
-        <FormKit name="nbbath" label="nombre de salle de bain" />
-        <FormKit name="nbsize" label="superficie" />
-      </FormKit>
-    </div>
-  </div>
 </template>
--- src/components/seco.vue
+++ src/components/seco.vue
@@ -1,46 +1,45 @@
 <script setup lang="ts">
-import { ref } from "@vue/reactivity";
-import { supabase, user } from "@/supabase";
+    import { ref } from "@vue/reactivity";
+    import { supabase, user } from "../supabase";
+    async function signIn(data, node) {
+      const { user, error } = await (nvlUtilisateur.value
+        ? supabase.auth.signUp(data)
+        : supabase.auth.signIn(data));
+      if (error) {
+        console.error(error);
+        node.setErrors([error.message]);
+      }
+    }
+    const nvlUtilisateur = ref(false);
+    </script>
+    
 
-async function signIn(data, node) {
-  const { user, error } = await (nvlUtilisateur.value
-    ? supabase.auth.signUp(data)
-    : supabase.auth.signIn(data));
-  if (error) {
-    console.error(error);
-    node.setErrors([error.message]);
-  }
-}
-const nvlUtilisateur = ref(false);
-</script>
-
-<template>
-  
-  <div class="flex justify-center bg-red-300">
-    <button  class="bg-red-700 hover:text-white m-4 p-6 text-xl font-bold rounded-lg" v-if="user" @pointerdown="supabase.auth.signOut( ) ">
-      Se déconnecter ({{ user.email }})
-    </button>
-    <FormKit
-      v-else
-      type="form"
-      :submit-attrs="{
-        classes: {
-          input:
-            'bg-white  m-4 p-4 rounded hover:bg-black hover:text-red-300',
-        },
-      }"
-      :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'"
-      @submit="signIn"
-    >
-      <FormKit name="email" label="Votre eMail" type="email" />
-      <FormKit name="password" label="Mot de passe" type="password " />
-      <formKit
-        label="Nouvel utilisateur ?"
-        class="flex flex-row"
-        name="nvlUtilisateur"
-        type="checkbox"
-        v-model="nvlUtilisateur"
-      />
-    </FormKit>
-  </div>
-</template>
+    <template>
+      <div>
+        <button v-if="user" @pointerdown="supabase.auth.signOut()">
+          Se déconnecter ({{ user.email }})
+        </button>
+        <FormKit
+          v-else
+          type="form"
+          :submit-label="nvlUtilisateur ? 'S\'inscrire' : 'Se connecter'"
+          @submit="signIn"
+          :config="{
+                        classes: {
+                                    input: 'p-1 rounded shadow-sm border bg-gray-100',
+                                    label: 'text-gray-600',
+                                },
+                    }"
+                    :submit-attrs="{ classes: { input: 'bg-red-600 mt-2 px-6 py-2 rounded-full text-center' } }"
+        >
+          <FormKit name="email" label="Votre eMail" type="email" />
+          <FormKit name="password" label="Mot de passe" type="password" />
+          <formKit
+            label="Nouvel utilisateur ?"
+            name="nvlUtilisateur"
+            type="checkbox"
+            v-model="nvlUtilisateur"
+          />
+        </FormKit>
+      </div>
+    </template>
